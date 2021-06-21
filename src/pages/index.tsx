02c58fc21eb3f375165042b58c5f7a31495d8437
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-21 16:55:08 -0300

    :package: Habilitando o auth context na página index e criando página profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import useAuth from "../hooks/useAuth";
+
 export default function Home() {
+  const { user, signIn } = useAuth();
+
   return (
-    <div>
+    <main>
       <h1>Let me ask</h1>
       <p>mas com NextJS ^^</p>
-    </div>
+
+      <div>
+        <button type="button" onClick={() => signIn()}>
+          Entrar com google
+        </button>
+      </div>
+    </main>
   );
 }
