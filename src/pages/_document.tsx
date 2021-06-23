02e2a706cad19d11edc89d26207ab53f20ceb7a0
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-23 15:27:43 -0300

    :package: Adicionando metatag de compartilhamento do facebook e imagem de capa para compartilhamento

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,21 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Facebook */}
+          <meta property="og:url"           content="https://letmeask-virid.vercel.app/" />
+          <meta property="og:type"          content="website" />
+          <meta property="og:title"         content="Letmeask" />
+          <meta property="og:description"   content="Sistema de ranqueamento de perguntas para lives!" />
+          <meta property="og:image"         content="https://letmeask-virid.vercel.app/letmeask.jpg" />
+          
+          {/* Google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto:wght@400;500&display=swap"
             rel="stylesheet"
           />
+
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
           <meta name="theme-color" content="#835AFD" />
         </Head>
