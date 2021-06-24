b1ebf6663ece840a214c8ab9a6eb95413cec23c8
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-24 08:18:20 -0300

    :package: Criando hook useRoom

---
+++ src/hooks/useRoom.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { database } from "../services/firebase";
+
+type QuestionType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+};
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighLighted: boolean;
+  }
+>;
+
+export default function useRoom(roomId: string) {
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions =
+        databaseRoom?.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighLighted: value.isHighLighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      setTitle(databaseRoom?.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  return { questions, title };
+}
