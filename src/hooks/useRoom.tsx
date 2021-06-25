6c8cee258f76083db1d35d9bcb44298cbff19848
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-25 09:54:25 -0300

    :wrench: Ajuste no nome do atributo highlighted

--- src/hooks/useRoom.tsx
+++ src/hooks/useRoom.tsx
@@ -10,7 +10,7 @@ type QuestionType = {
   };
   content: string;
   isAnswered: boolean;
-  isHighLighted: boolean;
+  isHighlighted: boolean;
   likeCount: number;
   likeId: string | undefined;
 };
@@ -22,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   };
   content: string;
   isAnswered: boolean;
-  isHighLighted: boolean;
+  isHighlighted: boolean;
   likes: Record<string, {
     authorId: string;
   }>
@@ -47,7 +47,7 @@ export default function useRoom(roomId: string) {
             id: key,
             content: value.content,
             author: value.author,
-            isHighLighted: value.isHighLighted,
+            isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
             likeCount: Object.values(value.likes ?? {}).length,
             likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
