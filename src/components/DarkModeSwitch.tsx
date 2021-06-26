72904480f4626b81910a0af53d6001e9672f2943
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-25 23:55:49 -0300

    :package: Estado do darkmode é salvo no localstorage

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../assets/styles/components/DarkModeSwitch.module.scss";
 
 export default function DarkModeSwitch() {
   const [isDarkModeActive, setIsDarkModeActive] = useState(false);
 
+  useEffect(() => {
+    if(localStorage.getItem("letmeask-darkMode")) {
+      activeDarkMode();
+    }
+  }, [isDarkModeActive]);
+
   function activeDarkMode() {
     document.getElementsByTagName("html")[0].classList.add("dark-mode");
+    localStorage.setItem("letmeask-darkMode", "true");
     setIsDarkModeActive(true);
   }
 
   function desactiveDarkMode() {
     document.getElementsByTagName("html")[0].classList.remove("dark-mode");
+    localStorage.removeItem("letmeask-darkMode");
     setIsDarkModeActive(false);
   }
 
