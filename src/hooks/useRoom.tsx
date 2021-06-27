56b6f23e96baec01caeb2b0c057fe4ac10aace4b
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-27 08:59:35 -0300

    :package: Definindo ordenação por likes e perguntas não respondidas

--- src/hooks/useRoom.tsx
+++ src/hooks/useRoom.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import useAuth from "../hooks/useAuth"
+import useAuth from "../hooks/useAuth";
 import { database } from "../services/firebase";
 
 type QuestionType = {
@@ -15,18 +15,24 @@ type QuestionType = {
   likeId: string | undefined;
 };
 
-type FirebaseQuestions = Record<string, {
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-  likes: Record<string, {
-    authorId: string;
-  }>
-}>;
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+    likes: Record<
+      string,
+      {
+        authorId: string;
+      }
+    >;
+  }
+>;
 
 export default function useRoom(roomId: string) {
   const { user } = useAuth();
@@ -50,13 +56,23 @@ export default function useRoom(roomId: string) {
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
             likeCount: Object.values(value.likes ?? {}).length,
-            likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0],
           };
         }
       );
 
+      const orderQuestionsByLikeCount = parsedQuestions.sort((roomA, roomB) =>
+        roomA.likeCount < roomB.likeCount ? 1 : roomA.likeCount > roomB.likeCount ? -1 : 0
+      );
+
+      const orderQuestionByNotAnswer = orderQuestionsByLikeCount.sort((roomA, roomB) => 
+        roomA.isAnswered > roomB.isAnswered ? 1 : roomA.isAnswered < roomB.isAnswered ? -1 : 0
+      );
+
       setTitle(databaseRoom?.title);
-      setQuestions(parsedQuestions);
+      setQuestions(orderQuestionByNotAnswer);
     });
 
     return () => {
